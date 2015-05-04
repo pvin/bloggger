4eb6a8908d7e95c999a643d5ddfa8e3662d1a95d
pvin <praaveenranganathan@gmail.com>
2015-05-04 17:48:15 +0530

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'posts#index'
-
+  get '/about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
